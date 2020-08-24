3b6cf5c4c667a2636ec0e7a5dcbd88338fbee85f
JoshuaYingWhat <joshuayingwhat@163.com>
2020-08-24 17:28:06 +0800

    chapter 02

---
+++ chapter_02/chapter_02.c
@@ -0,0 +1,5 @@
+//
+// Created by JoshuaYingWhat on 2020/8/24.
+//
+
+//#include "chapter_02.h"
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "chapter_02/chapter_02.h"
+
+int main() {
+
+    chapter_02();
+
+    return 0;
+}
