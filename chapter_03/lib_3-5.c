f013a13d5cc0bc4ce9d441a10b7ddc080aaffe9c
joshuayingwhat <joshuayingwhat@163.com>
2020-09-04 10:42:57 +0800

    第三章 3.5的练习

---
+++ chapter_03/lib_3-5.c
@@ -0,0 +1,32 @@
+//
+// Created by JoshuaYingWhat on 2020/9/4.
+//
+
+#include "lib_3-5.h"
+
+//通过得到的汇编代码 写出decode1的代码逻辑
+//decode1:
+//movq (%rdi), %r8
+//movq (%rsi), %rcx
+//movq (%rdx), %rax
+//movq   %r8,  (%rsi)
+//movq %rcx,   (%rdx)
+//movq %rax,  (%rdi)
+//ret
+//xp,yp,zp 分别存储于%rdi,%rsi,%rdx
+void decode1(long *xp, long *yp, long *zp) {
+
+    long r8 = *xp;// r8 存储于%r8中
+
+    long rcx = *yp;// rcx存储于%rcx中
+
+    long rax = *zp;//rax存储于%rax中
+
+     *yp = r8;
+
+    *zp = rcx;
+
+    *xp = rax;
+
+    return;
+}
---
+++ chapter_03/lib_3-5.h
@@ -0,0 +1,10 @@
+//
+// Created by JoshuaYingWhat on 2020/9/4.
+//
+
+#ifndef CSAPP_C_LIB_3_5_H
+#define CSAPP_C_LIB_3_5_H
+
+void decode1(long *xp, long *yp, long *zp);
+
+#endif //CSAPP_C_LIB_3_5_H
